30a2843cfbd647e26833d3bd0a3b8ed17f045c5a
Chris Clark <xchrisck@gmail.com>
2023-06-01 15:46:52 -0500

    blahhh

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,18 +1,3 @@
-import "./ShoppingList.css";
-
-function ShoppingList(props) {
-  console.log(props);
-
-  return (
-    <div id="shopping-list">
-      <button>Reset</button>
-      <button>Clear</button>
-      {props.groceryList.map((item) => (
-        <div className="grocery-item" id={item.id}>
-          {item.item} <br></br> {item.quantity}&nbsp;{item.unit}
-          <br></br>
-          <button className="buy-btn">Buy</button>
-          <button className="remove-btn">Remove</button>
 import { useState } from "react";
 import ShoppingItem from "../ShoppingItem/ShoppingItem";
 import ResetBtn from "../ResetBtn/ResetBtn";
@@ -30,10 +15,8 @@ function ShoppingList (props) {
             )
             )}
         </div>
-
         </>
     )
-
 }
 
 export default ShoppingList;
