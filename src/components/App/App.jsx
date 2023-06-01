b0142d2ff7f8f0755fdc43ec10326c150e39fb5f
Adam Oberle <oberle.adam@gmail.com>
2023-06-01 11:57:36 -0500

    stuff

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
-
+import axios from 'axios';
 
 function App() {
+
+    const [groceryList, setGroceryList] = useState([]);
+
+    const getGroceryList = () => {
+        axios.get('/groceries').then(response => {
+            setGroceryList(response.data);
+        }).catch(error => {
+            console.log('error is:', error)
+            alert('Error with GET request')
+        })
+    }
+
+    useEffect(() => {
+        getGroceryList();
+      }, []);
+
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
+                <ShoppingList groceryList={groceryList}/>
             </main>
         </div>
     );
