8ef0c6c1536b33cdfbd2116b74976dcc29bef85b
Will Richardson <rich1051@umn.edu>
2023-06-01 15:07:47 -0500

    Shopping List .css

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,16 +1,22 @@
-function ShoppingList (props) {
-    console.log(props)
+import "./ShoppingList.css";
 
-    return (
-        <div id='shopping-list'>
-            <button>Reset</button>
-            <button>Clear</button>
-            {props.groceryList.map(item => (
-                <div className='grocery-item' id={item.id}>{item.item} <br></br> {item.quantity}{item.unit}<br></br><button>Buy</button><button>Remove</button></div>
-            )
-            )}
+function ShoppingList(props) {
+  console.log(props);
+
+  return (
+    <div id="shopping-list">
+      <button>Reset</button>
+      <button>Clear</button>
+      {props.groceryList.map((item) => (
+        <div className="grocery-item" id={item.id}>
+          {item.item} <br></br> {item.quantity}&nbsp;{item.unit}
+          <br></br>
+          <button className="buy-btn">Buy</button>
+          <button className="remove-btn">Remove</button>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default ShoppingList;
